ed10a9f113d1b546b9d77194817c6919e7fd6571
Beraat Öztorun <beraat.oztorun@std.hku.edu.tr>
2023-06-27 03:48:43 +0300

    new project

--- hexadecimal.c
+++ hexadecimal.c
@@ -6,7 +6,7 @@ int main() {
     printf("Bir ondalik sayi girin: ");
     scanf("%lf", &sayi);
 
-    printf("Girilen sayi on altilik (hexadecimal) biçimde: %a\n", sayi);
+    printf("Girilen sayi on altilik (hexadecimal) bicimde: %a\n", sayi);
 
     return 0;
 }
